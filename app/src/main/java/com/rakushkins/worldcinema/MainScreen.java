4ce799c332ebed89b3e9c512953ee94d05a76915
rakushka-san <vladislavmohov@mail.ru>
2022-02-04 14:48:50 +0300

    "Module 2"

--- app/src/main/java/com/rakushkins/worldcinema/MainScreen.java
+++ app/src/main/java/com/rakushkins/worldcinema/MainScreen.java
@@ -2,8 +2,11 @@ package com.rakushkins.worldcinema;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -12,5 +15,15 @@ public class MainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
         setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        Button exit_btn = (Button) findViewById(R.id.mainscr_exit_btn);
+        exit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toSignInScreen = new Intent(MainScreen.this, SignInScreen.class);
+                startActivity(toSignInScreen);
+                finish();
+            }
+        });
     }
 }
